ce7a9d51944d0cf9e2d3b51a62675b58308a6806
= <=>
2023-08-31 17:45:56 +0530

    ArrayList and LinkedList program has been loaded

---
+++ AddContentLinkedList.java
@@ -0,0 +1,16 @@
+package basicProgram;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddContentLinkedList {
+    public static void main(String[] args) {
+        List<Integer> l = new LinkedList<Integer>();
+        l.add(10);
+        l.add(30);
+        l.add(40);
+        System.out.println(l);
+    }
+
+}
